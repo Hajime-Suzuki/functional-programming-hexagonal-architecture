a15cffb38a47db98a8febf6a25d89148f247509c
Hajime Suzuki <hajime.doublebass@gmail.com>
2020-10-03 10:11:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Survey app API to show fp-ts examples
+
+### Goal
+
+- one day project
+- hexagonal architecture with fp-ts
+
+### Functionalities
+
+- create survey
+- update survey
+- create response
+- read all response
+
+### Local Development
+
+- dynamodb local is used, so Java Runtime Engine (JRE) version 6.x or newer is required (https://www.serverless.com/plugins/serverless-dynamodb-local)
+- run `yarn install`
+- run `yarn dev`
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const slsw = require('serverless-webpack')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  resolve: {
+    extensions: ['.json', '.ts'],
+    alias: {
+      '@modules': path.resolve(__dirname, './src/modules'),
+    },
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  externals: [nodeExternals()],
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: { transpileOnly: true, configFile: 'tsconfig.json' },
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: true,
+      eslint: {
+        enabled: true,
+        files: './src/**/*.{ts,tsx}'
+      }
+    }),
+  ],
+}
